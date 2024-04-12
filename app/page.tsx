697fd294b1b36369494613415a8a3730f30c300c
a3chron <121883800+kurtschambach@users.noreply.github.com>
2024-04-12 16:34:12 +0200

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white text-black dark:bg-zinc-950 dark:text-zinc-100">
+      <h1 className="font-plex text-xl">NextJS Template</h1>
+    </main>
+  );
+}
